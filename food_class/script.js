df0d694cc92a02702f283c326fecd8e015969eb5
alexey100196 <alexeysobolev071@gmail.com>
2019-06-10 09:35:57 +0300

    add food_class

---
+++ food_class/script.js
@@ -0,0 +1,99 @@
+
+class Food {
+    constructor(name , price, ingredients) {
+      this.name = name;
+      this.price = price;
+      this.ingredients = ingredients;
+    }
+    addCount() {
+      this.count++
+      console.log(`${this.count}`)
+    }
+    orderIsReady() {
+      console.log(`start cooking ${this.name}:`)
+      setTimeout(() => {
+        console.log(`${this.name} cooked`)
+        this.count++
+        console.log(`${this.name}: ${this.count}`)
+      }, this.cookTime)
+    }
+    addPizzaCount() {
+      setTimeout(() => {
+        document.querySelector('.cook_count-pizza').innerHTML = this.count
+        // console.log(this.count)
+      }, this.cookTime)
+  }
+    addBurgerCount() {
+      setTimeout(() => {
+        document.querySelector('.cook_count-burger').innerHTML = this.count
+        // console.log(this.count)
+      }, this.cookTime)
+    }
+    addTacoCount() {
+      setTimeout(() => {
+        document.querySelector('.cook_count-taco').innerHTML = this.count
+        // console.log(this.count)
+      }, this.cookTime)
+  }
+
+  giveOrder() {
+   this.count = 0
+  }
+}
+class Pizza extends Food {
+  constructor(name , price, ingredients, count, cookTime =1500) {
+    super (name, price, ingredients,);
+    this.count = count;
+    this.cookTime = cookTime
+  }  
+}
+class Burger extends Food {
+  constructor(name , price, ingredients, count, cookTime = 500) {
+    super (name, price, ingredients);
+    this.count = count;
+    this.cookTime = cookTime
+  }
+}
+class Taco extends Food {
+  constructor(name , price, ingredients, count, cookTime = 1000) {
+    super (name, price, ingredients);
+    this.count = count;
+    this.cookTime = cookTime
+    console.log(this.count)
+  }
+}
+let pizza = new Pizza('pizza', 14, ['mozzarella ' , 'papironi', 'lamborginy'], 0);
+let burger = new Burger('Burger', 10, ['bred ' , 'cheese', 'cucumber'], 0);
+let taco = new Taco('Taco', 12,  ['arr1 ' , 'arr2', 'arr3'], 0);
+
+document.querySelector('#cook-pizza').addEventListener('click', function(){
+    pizza.orderIsReady()
+    pizza.addPizzaCount()
+  });
+document.querySelector('#cook-burger').addEventListener('click', function() {
+    burger.orderIsReady()
+    burger.addBurgerCount()
+  })
+document.querySelector('#cook-taco').addEventListener('click',  function() {
+    taco.orderIsReady()
+    taco.addTacoCount()
+  });
+
+      
+let textCount = document.querySelector('.cook_count') 
+
+// setInterval(function() {
+//   if (textCount.innerHTML > 0 ) {
+//     console.log('test');
+//   }
+// }, 1000)
+
+document.querySelector('.btn_order').addEventListener('click', function(){
+  if (textCount.innerHTML > 0 ) {
+    textCount.innerHTML = 0;
+    pizza.giveOrder()
+    document.querySelector('#orderDone').innerHTML = textCount.innerHTML
+  }
+});
+
+
